1fae916384d509df93ee8698c4b1ad0aa7a0aba4
modmuss50 <modmuss50@gmail.com>
2018-11-05 22:15:00 +0000

    fix run client, fix needed as it now runs in a named env

--- src/main/java/net/fabricmc/loom/task/RunClientTask.java
+++ src/main/java/net/fabricmc/loom/task/RunClientTask.java
@@ -79,6 +79,7 @@ public class RunClientTask extends JavaExec {
 	public List<String> getJvmArgs() {
 		LoomGradleExtension extension = this.getProject().getExtensions().getByType(LoomGradleExtension.class);
 		List<String> args = new ArrayList<>();
+		args.add("-Dfabric.development=true");
 		return args;
 	}
 

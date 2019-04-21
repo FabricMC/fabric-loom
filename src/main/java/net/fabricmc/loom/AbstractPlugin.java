253c2ed15e6350479b83258cf3bf145058224626
Adrian Siekierka <kontakt@asie.pl>
2019-04-21 23:50:28 +0200

    support appending to existing <dependencies> keys in maven-publish hook; in came a buildscript uses .withXml, say

--- src/main/java/net/fabricmc/loom/AbstractPlugin.java
+++ src/main/java/net/fabricmc/loom/AbstractPlugin.java
@@ -26,34 +26,22 @@ package net.fabricmc.loom;
 
 import com.google.common.collect.ImmutableMap;
 import groovy.util.Node;
-import groovy.util.NodeList;
-import groovy.xml.QName;
 import net.fabricmc.loom.providers.MappingsProvider;
 import net.fabricmc.loom.providers.MinecraftProvider;
 import net.fabricmc.loom.providers.ModRemapperProvider;
 import net.fabricmc.loom.task.RemapJar;
 import net.fabricmc.loom.task.RemapSourcesJar;
-import net.fabricmc.loom.util.Constants;
-import net.fabricmc.loom.util.LoomDependencyManager;
-import net.fabricmc.loom.util.NestedJars;
-import net.fabricmc.loom.util.SetupIntelijRunConfigs;
+import net.fabricmc.loom.util.*;
 import org.gradle.api.*;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
-import org.gradle.api.artifacts.ModuleDependency;
-import org.gradle.api.artifacts.ProjectDependency;
 import org.gradle.api.artifacts.dsl.DependencyHandler;
-import org.gradle.api.artifacts.maven.Conf2ScopeMappingContainer;
 import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
-import org.gradle.api.plugins.MavenPlugin;
-import org.gradle.api.plugins.MavenPluginConvention;
 import org.gradle.api.publish.Publication;
 import org.gradle.api.publish.PublishingExtension;
 import org.gradle.api.publish.maven.MavenPublication;
-import org.gradle.api.publish.maven.tasks.AbstractPublishToMaven;
-import org.gradle.api.publish.maven.tasks.GenerateMavenPom;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.bundling.AbstractArchiveTask;
 import org.gradle.api.tasks.compile.JavaCompile;
@@ -63,10 +51,9 @@ import org.gradle.plugins.ide.idea.model.IdeaModel;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class AbstractPlugin implements Plugin<Project> {
@@ -313,9 +300,24 @@ public class AbstractPlugin implements Plugin<Project> {
 						if (publication instanceof MavenPublication) {
 							((MavenPublication) publication).pom((pom) -> {
 								pom.withXml((xml) -> {
-									Node dependencies = xml.asNode().appendNode("dependencies");
+									Node dependencies = GroovyXmlUtil.getOrCreateNode(xml.asNode(), "dependencies");
+									Set<String> foundArtifacts = new HashSet<>();
+
+									GroovyXmlUtil.childrenNodesStream(dependencies)
+											.filter((n) -> "dependency".equals(n.name()))
+											.forEach((n) -> {
+												Optional<Node> groupId = GroovyXmlUtil.getNode(n, "groupId");
+												Optional<Node> artifactId = GroovyXmlUtil.getNode(n, "artifactId");
+												if (groupId.isPresent() && artifactId.isPresent()) {
+													foundArtifacts.add(groupId.get().text() + ":" + artifactId.get().text());
+												}
+											});
 
 									for (Dependency dependency : compileModsConfig.getAllDependencies()) {
+										if (foundArtifacts.contains(dependency.getGroup() + ":" + dependency.getName())) {
+											continue;
+										}
+
 										Node depNode = dependencies.appendNode("dependency");
 										depNode.appendNode("groupId", dependency.getGroup());
 										depNode.appendNode("artifactId", dependency.getName());

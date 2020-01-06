ea5cc2cea6f9b3c6b9a747608194baabee3f66e5
natanfudge <natan.lifsiz@gmail.com>
2020-01-06 20:29:22 +0200

    the things

---
+++ src/main/java/net/fabricmc/loom/task/fernflower/IncrementalDecompilation.java
@@ -0,0 +1,4 @@
+package net.fabricmc.loom.task.fernflower;
+
+public class IncrementalDecompilation {
+}
---
+++ src/test/java/net/minecraft/fabricmc/loom/IncrementalDecompilationTest.java
@@ -0,0 +1,4 @@
+package net.minecraft.fabricmc.loom;
+
+public class IncrementalDecompilationTest {
+}

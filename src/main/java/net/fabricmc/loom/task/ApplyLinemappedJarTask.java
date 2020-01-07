8baa9d6b8f3f56489c27c92e8da07739f140d3e1
natanfudge <natan.lifsiz@gmail.com>
2020-01-08 01:32:06 +0200

    fix

--- src/main/java/net/fabricmc/loom/task/ApplyLinemappedJarTask.java
+++ src/main/java/net/fabricmc/loom/task/ApplyLinemappedJarTask.java
@@ -1,15 +1,42 @@
-package net.fabricmc.loom.task;
+/*
+ * This file is part of fabric-loom, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016, 2017, 2018 FabricMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
-import net.fabricmc.loom.LoomGradleExtension;
-import org.gradle.api.tasks.InputFile;
-import org.gradle.api.tasks.OutputFile;
-import org.gradle.api.tasks.TaskAction;
+package net.fabricmc.loom.task;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Optional;
+
+import org.gradle.api.tasks.InputFile;
+import org.gradle.api.tasks.OutputFile;
+import org.gradle.api.tasks.TaskAction;
+
+import net.fabricmc.loom.LoomGradleExtension;
 
 public class ApplyLinemappedJarTask extends AbstractLoomTask {
 	private Object mappedJar;
@@ -25,7 +52,6 @@ public class ApplyLinemappedJarTask extends AbstractLoomTask {
 		return getProject().file(linemappedJar);
 	}
 
-
 	public void setMappedJar(Object mappedJar) {
 		this.mappedJar = mappedJar;
 	}
@@ -34,10 +60,11 @@ public class ApplyLinemappedJarTask extends AbstractLoomTask {
 		this.linemappedJar = linemappedJar;
 	}
 
-	public static Path jarsBeforeLinemapping(LoomGradleExtension extension) {
-		return Paths.get(extension.getUserCache().getPath(), "jarsBeforeLinemapping");
+	public static Path jarsBeforeLinemapping(LoomGradleExtension extension) throws IOException {
+		Path path = Paths.get(extension.getUserCache().getPath(), "jarsBeforeLinemapping");
+		Files.createDirectories(path);
+		return path;
 	}
-
 
 	@TaskAction
 	public void run() {
@@ -47,15 +74,13 @@ public class ApplyLinemappedJarTask extends AbstractLoomTask {
 		if (Files.exists(linemappedJarPath)) {
 			try {
 				Files.createDirectories(jarsBeforeLinemapping(getExtension()));
+				Path notLinemappedJar = jarsBeforeLinemapping(getExtension()).resolve(mappedJarPath.getFileName());
 				// The original jars without the linemapping needs to be saved for incremental decompilation
-				Files.copy(mappedJarPath, jarsBeforeLinemapping(getExtension()).resolve(mappedJarPath.getFileName()));
-				Files.deleteIfExists(mappedJarPath);
-				Files.copy(linemappedJarPath, mappedJarPath);
+				if (!Files.exists(notLinemappedJar)) Files.copy(mappedJarPath, notLinemappedJar);
+				Files.copy(linemappedJarPath, mappedJarPath, StandardCopyOption.REPLACE_EXISTING);
 			} catch (IOException e) {
 				throw new RuntimeException(e);
 			}
 		}
 	}
-
-
 }

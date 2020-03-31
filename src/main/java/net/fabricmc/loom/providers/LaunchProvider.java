737a920b1b0569b61f427fc723cdbf5a99478d15
modmuss50 <modmuss50@gmail.com>
2020-03-31 20:28:08 +0100

    Use a custom dev time log4j config. Enables color output and improves overall logging. Color output is not enabled by default for eclipse users

--- src/main/java/net/fabricmc/loom/providers/LaunchProvider.java
+++ src/main/java/net/fabricmc/loom/providers/LaunchProvider.java
@@ -26,7 +26,9 @@ package net.fabricmc.loom.providers;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -49,6 +51,7 @@ public class LaunchProvider extends DependencyProvider {
 	public void provide(DependencyInfo dependency, Consumer<Runnable> postPopulationScheduler) throws IOException {
 		final LaunchConfig launchConfig = new LaunchConfig()
 				.property("fabric.development", "true")
+				.property("log4j.configurationFile", getLog4jConfigFile().getAbsolutePath())
 
 				.property("client", "java.library.path", getExtension().getNativesDirectory().getAbsolutePath())
 				.property("client", "org.lwjgl.librarypath", getExtension().getNativesDirectory().getAbsolutePath())
@@ -58,9 +61,29 @@ public class LaunchProvider extends DependencyProvider {
 				.argument("client", "--assetsDir")
 				.argument("client", new File(getExtension().getUserCache(), "assets").getAbsolutePath());
 
+		//Disable ansi color output for eclipse users
+		if (!getExtension().forceAnsiConsole && (new File(getProject().getRootDir(), ".project").exists() || new File(getProject().getRootDir(), ".classpath").exists())) {
+			launchConfig.property("fabric.log.disableAnsi", "true");
+		}
+
+		writeLog4jConfig();
 		FileUtils.writeStringToFile(getExtension().getDevLauncherConfig(), launchConfig.asString(), StandardCharsets.UTF_8);
 
 		addDependency("net.fabricmc:dev-launch-injector:" + Constants.DEV_LAUNCH_INJECTOR_VERSION, "runtimeOnly");
+		addDependency("net.minecrell:terminalconsoleappender:" + Constants.TERMINAL_CONSOLE_APPENDER_VERSION, "runtimeOnly");
+	}
+
+	private File getLog4jConfigFile() {
+		return new File(getExtension().getDevLauncherConfig().getParentFile(), "log4j.xml");
+	}
+
+	private void writeLog4jConfig() {
+		try (InputStream is = LaunchProvider.class.getClassLoader().getResourceAsStream("log4j2.fabric.xml")) {
+			Files.deleteIfExists(getLog4jConfigFile().toPath());
+			Files.copy(is, getLog4jConfigFile().toPath());
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to generate log4j config", e);
+		}
 	}
 
 	@Override

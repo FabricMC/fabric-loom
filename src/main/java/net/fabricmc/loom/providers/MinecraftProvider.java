39e6abb7eeb54d1feef12989fb0be6571e72a329
natanfudge <natan.lifsiz@gmail.com>
2020-07-17 12:13:54 +0300

    spaaace

--- src/main/java/net/fabricmc/loom/providers/MinecraftProvider.java
+++ src/main/java/net/fabricmc/loom/providers/MinecraftProvider.java
@@ -68,7 +68,6 @@ public class MinecraftProvider extends DependencyProvider {
 	@Override
 	public void provide(DependencyInfo dependency, Consumer<Runnable> postPopulationScheduler) throws Exception {
 		minecraftVersion = dependency.getDependency().getVersion();
-
 		boolean offline = getProject().getGradle().getStartParameter().isOffline();
 
 		initFiles();

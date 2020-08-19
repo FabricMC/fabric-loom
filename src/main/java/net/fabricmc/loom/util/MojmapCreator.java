d4de68f112aa96ce53f25ba43cfe768f36ebba8d
Ramid Khan <ramidzkh@gmail.com>
2020-08-20 07:24:32 +1000

    Use V2

--- src/main/java/net/fabricmc/loom/util/MojmapCreator.java
+++ src/main/java/net/fabricmc/loom/util/MojmapCreator.java
@@ -24,10 +24,11 @@
 
 package net.fabricmc.loom.util;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.Writer;
 import java.net.URI;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -41,14 +42,14 @@ import java.util.function.Consumer;
 
 import com.google.common.io.MoreFiles;
 import com.google.gson.Gson;
-import org.cadixdev.bombe.type.signature.FieldSignature;
-import org.cadixdev.bombe.type.signature.MethodSignature;
 import org.cadixdev.lorenz.MappingSet;
+import org.cadixdev.lorenz.io.TextMappingsWriter;
 import org.cadixdev.lorenz.io.proguard.ProGuardReader;
 import org.cadixdev.lorenz.model.ClassMapping;
 import org.cadixdev.lorenz.model.FieldMapping;
 import org.cadixdev.lorenz.model.InnerClassMapping;
 import org.cadixdev.lorenz.model.MethodMapping;
+import org.cadixdev.lorenz.model.MethodParameterMapping;
 import org.cadixdev.lorenz.model.TopLevelClassMapping;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Dependency;
@@ -71,81 +72,86 @@ public class MojmapCreator {
 
 	public static Dependency mojmap(Project project, LoomGradleExtension extension, String minecraftVersionRaw) throws IOException {
 		File userCache = extension.getUserCache();
-
 		File mappingsDir = new File(userCache, "mappings");
 		File steps = new File(mappingsDir, "steps");
 
-		File manifests = new File(userCache, "version_manifest.json");
-		DownloadUtil.downloadIfChanged(new URL("https://launchermeta.mojang.com/mc/game/version_manifest.json"), manifests, project.getLogger());
-		String versionManifest = MoreFiles.asCharSource(manifests.toPath(), StandardCharsets.UTF_8).read();
-		ManifestVersion mcManifest = GSON.fromJson(versionManifest, ManifestVersion.class);
-
-		ManifestVersion.Versions version = mcManifest.versions.stream().filter(v -> v.id.equals(minecraftVersionRaw)).findAny().get();
+		ManifestVersion.Versions version = findVersion(project, userCache, minecraftVersionRaw);
 		String id = version.id;
 
 		File mappingsJar = new File(mappingsDir, "mojmap-" + id + ".jar");
 
 		if (!mappingsJar.exists()) {
-			File minecraftJson = new File(userCache, "minecraft-" + id + "-info.json");
-			DownloadUtil.downloadIfChanged(new URL(version.url), minecraftJson, project.getLogger());
+			MappingSet officialToNamed;
+			MappingSet intermediaryToOfficial;
 
-			MinecraftVersionInfo info = GSON.fromJson(MoreFiles.asCharSource(minecraftJson.toPath(), StandardCharsets.UTF_8).read(), MinecraftVersionInfo.class);
+			// Download and parse Proguard logs
+			{
+				File minecraftJson = new File(userCache, "minecraft-" + id + "-info.json");
+				DownloadUtil.downloadIfChanged(new URL(version.url), minecraftJson, project.getLogger());
 
-			File clientMappings = new File(steps, "mojmap-" + id + "-client.txt");
-			File serverMappings = new File(steps, "mojmap-" + id + "-server.txt");
-			String clientMappingsUrl = info.downloads.get("client_mappings").url;
-			String serverMappingsUrl = info.downloads.get("server_mappings").url;
+				MinecraftVersionInfo info = GSON.fromJson(MoreFiles.asCharSource(minecraftJson.toPath(), StandardCharsets.UTF_8).read(), MinecraftVersionInfo.class);
 
-			DownloadUtil.downloadIfChanged(new URL(clientMappingsUrl), clientMappings, project.getLogger());
-			DownloadUtil.downloadIfChanged(new URL(serverMappingsUrl), serverMappings, project.getLogger());
+				File clientMappings = new File(steps, "mojmap-" + id + "-client.txt");
+				File serverMappings = new File(steps, "mojmap-" + id + "-server.txt");
+				String clientMappingsUrl = info.downloads.get("client_mappings").url;
+				String serverMappingsUrl = info.downloads.get("server_mappings").url;
 
-			MappingSet mappings = MappingSet.create();
+				DownloadUtil.downloadIfChanged(new URL(clientMappingsUrl), clientMappings, project.getLogger());
+				DownloadUtil.downloadIfChanged(new URL(serverMappingsUrl), serverMappings, project.getLogger());
 
-			try (
-					ProGuardReader clientReader = new ProGuardReader(new FileReader(clientMappings));
-					ProGuardReader serverReader = new ProGuardReader(new FileReader(serverMappings))) {
-				clientReader.read(mappings);
-				serverReader.read(mappings);
+				MappingSet mappings = MappingSet.create();
+
+				try (
+						ProGuardReader clientReader = new ProGuardReader(new FileReader(clientMappings));
+						ProGuardReader serverReader = new ProGuardReader(new FileReader(serverMappings))) {
+					clientReader.read(mappings);
+					serverReader.read(mappings);
+				}
+
+				officialToNamed = mappings.reverse();
 			}
 
-			MappingSet officialToNamed = mappings.reverse();
-			MappingSet officialToIntermediary;
+			// Download and parse intermediaries
+			{
+				File intermediaryJar = new File(steps, "v2-intermediary-" + id + ".jar");
+				DownloadUtil.downloadIfChanged(new URL(extension.getIntermediaryUrl().apply(id)), intermediaryJar, project.getLogger());
 
-			File intermediaryJar = new File(steps, "v2-intermediary-" + id + ".jar");
-			DownloadUtil.downloadIfChanged(new URL(extension.getIntermediaryUrl().apply(id)), intermediaryJar, project.getLogger());
-
-			try (FileSystem fileSystem = FileSystems.newFileSystem(intermediaryJar.toPath(), CLASS_LOADER)) {
-				officialToIntermediary = TinyMappingFormat.DETECT.createReader(fileSystem.getPath("mappings/mappings.tiny"), "official", "intermediary").read();
+				try (FileSystem fileSystem = FileSystems.newFileSystem(intermediaryJar.toPath(), CLASS_LOADER)) {
+					intermediaryToOfficial = TinyMappingFormat.DETECT.createReader(fileSystem.getPath("mappings/mappings.tiny"), "intermediary", "official").read();
+				}
 			}
 
+			MappingSet intermediaryToNamed = MappingSet.create();
+
+			// Merging. Don't use MappingSet#merge
+			iterateClasses(intermediaryToOfficial, i2oClassMapping -> {
+				officialToNamed.getClassMapping(i2oClassMapping.getFullDeobfuscatedName()).ifPresent(o2nClassMapping -> {
+					ClassMapping<?, ?> c = intermediaryToNamed.getOrCreateClassMapping(i2oClassMapping.getFullObfuscatedName())
+							.setDeobfuscatedName(o2nClassMapping.getFullDeobfuscatedName());
+
+					for (FieldMapping i2oFieldMapping : i2oClassMapping.getFieldMappings()) {
+						o2nClassMapping.getFieldMapping(i2oFieldMapping.getDeobfuscatedName()).ifPresent(o2nFieldMapping -> {
+							c.getOrCreateFieldMapping(i2oFieldMapping.getSignature())
+									.setDeobfuscatedName(o2nFieldMapping.getDeobfuscatedName());
+						});
+					}
+
+					for (MethodMapping i2oMethodMapping : i2oClassMapping.getMethodMappings()) {
+						o2nClassMapping.getMethodMapping(i2oMethodMapping.getDeobfuscatedSignature()).ifPresent(o2nMethodMapping -> {
+							c.getOrCreateMethodMapping(i2oMethodMapping.getSignature())
+									.setDeobfuscatedName(o2nMethodMapping.getDeobfuscatedName());
+						});
+					}
+				});
+			});
+
+			// Write
 			try (FileSystem fileSystem = FileSystems.newFileSystem(URI.create("jar:" + mappingsJar.toURI().toURL()), CREATE)) {
 				Path path = fileSystem.getPath("mappings", "mappings.tiny");
 				Files.createDirectories(path.getParent());
 
-				try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(path))) {
-					writer.println("v1\tofficial\tintermediary\tnamed");
-
-					iterateClasses(officialToIntermediary, intermediaryClass -> {
-						String officialName = intermediaryClass.getFullObfuscatedName();
-						ClassMapping<?, ?> mojmapClass = officialToNamed.getClassMapping(officialName).map(x -> (ClassMapping) x).orElse(intermediaryClass);
-						writer.println("CLASS\t" + officialName + "\t" + intermediaryClass.getFullDeobfuscatedName() + "\t" + mojmapClass.getFullDeobfuscatedName());
-
-						for (FieldMapping intermediaryField : intermediaryClass.getFieldMappings()) {
-							FieldSignature signature = intermediaryField.getSignature();
-							FieldMapping mojmapField = mojmapClass.getFieldMapping(signature.getName()).orElse(intermediaryField);
-
-							writer.println("FIELD\t" + officialName + "\t" + signature.getType().get() + "\t" + intermediaryField.getObfuscatedName() + "\t" + intermediaryField.getDeobfuscatedName() + "\t" + mojmapField.getDeobfuscatedName());
-						}
-
-						for (MethodMapping intermediaryMethod : intermediaryClass.getMethodMappings()) {
-							MethodSignature signature = intermediaryMethod.getSignature();
-							MethodMapping mojmapMethod = mojmapClass.getMethodMapping(signature).orElse(intermediaryMethod);
-
-							writer.println("METHOD\t" + officialName + "\t" + signature.getDescriptor() + "\t" + intermediaryMethod.getObfuscatedName() + "\t" + intermediaryMethod.getDeobfuscatedName() + "\t" + mojmapMethod.getDeobfuscatedName());
-						}
-					});
-
-					writer.println();
+				try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+					new TinyWriter(writer, "intermediary", "named").write(intermediaryToNamed);
 				}
 			}
 		}
@@ -170,6 +176,15 @@ public class MojmapCreator {
 		};
 	}
 
+	private static ManifestVersion.Versions findVersion(Project project, File userCache, String raw) throws IOException {
+		File manifests = new File(userCache, "version_manifest.json");
+		DownloadUtil.downloadIfChanged(new URL("https://launchermeta.mojang.com/mc/game/version_manifest.json"), manifests, project.getLogger());
+		String versionManifest = MoreFiles.asCharSource(manifests.toPath(), StandardCharsets.UTF_8).read();
+		ManifestVersion mcManifest = GSON.fromJson(versionManifest, ManifestVersion.class);
+
+		return mcManifest.versions.stream().filter(v -> v.id.equals(raw)).findAny().get();
+	}
+
 	private static void iterateClasses(MappingSet mappings, Consumer<ClassMapping<?, ?>> consumer) {
 		for (TopLevelClassMapping classMapping : mappings.getTopLevelClassMappings()) {
 			iterateClass(classMapping, consumer);
@@ -181,6 +196,41 @@ public class MojmapCreator {
 
 		for (InnerClassMapping innerClassMapping : classMapping.getInnerClassMappings()) {
 			iterateClass(innerClassMapping, consumer);
+		}
+	}
+
+	private static class TinyWriter extends TextMappingsWriter {
+		private final String namespaceFrom;
+		private final String namespaceTo;
+
+		protected TinyWriter(Writer writer, String namespaceFrom, String namespaceTo) {
+			super(writer);
+			this.namespaceFrom = namespaceFrom;
+			this.namespaceTo = namespaceTo;
+		}
+
+		@Override
+		public void write(MappingSet mappings) {
+			writer.println("tiny\t2\t0\t" + namespaceFrom + "\t" + namespaceTo);
+
+			iterateClasses(mappings, classMapping -> {
+				writer.println("c\t" + classMapping.getFullObfuscatedName() + "\t" + classMapping.getFullDeobfuscatedName());
+
+				for (FieldMapping fieldMapping : classMapping.getFieldMappings()) {
+					fieldMapping.getType().ifPresent(fieldType -> {
+						writer.println("\tf\t" + fieldType + "\t" + fieldMapping.getObfuscatedName() + "\t" + fieldMapping.getDeobfuscatedName());
+					});
+				}
+
+				for (MethodMapping methodMapping : classMapping.getMethodMappings()) {
+					writer.println("\tm\t" + methodMapping.getSignature().getDescriptor() + "\t" + methodMapping.getObfuscatedName() + "\t" + methodMapping.getDeobfuscatedName());
+
+					for (MethodParameterMapping parameterMapping : methodMapping.getParameterMappings()) {
+						// Lorenz doesn't support parameters having an obfuscated name
+						writer.println("\t\tp\t" + parameterMapping.getIndex() + "\t\t\t" + parameterMapping.getDeobfuscatedName());
+					}
+				}
+			});
 		}
 	}
 }

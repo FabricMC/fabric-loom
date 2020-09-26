c647bf7a309c9de08ae4d4a71996245528388326
modmuss50 <modmuss50@gmail.com>
2020-09-26 16:44:23 +0100

    Fix bad merge

--- src/main/java/net/fabricmc/loom/providers/LaunchProvider.java
+++ src/main/java/net/fabricmc/loom/providers/LaunchProvider.java
@@ -101,7 +101,7 @@ public class LaunchProvider extends DependencyProvider {
 
 	private void writeRemapClassPath() {
 		List<String> inputConfigurations = new ArrayList<>();
-		inputConfigurations.add(Constants.MINECRAFT_DEPENDENCIES);
+		inputConfigurations.add(Constants.Configurations.MINECRAFT_DEPENDENCIES);
 		inputConfigurations.addAll(Constants.MOD_COMPILE_ENTRIES.stream().map(RemappedConfigurationEntry::getSourceConfiguration).collect(Collectors.toList()));
 
 		List<File> remapClasspath = new ArrayList<>();

86577525cdc72198727490dff37530dfaf013c88
Hephaestus-Dev <54509200+Hephaestus-Dev@users.noreply.github.com>
2020-10-08 12:42:07 -0700

    Mark auto-generated mods with a custom value (#271)
    * Make the barebones `fabric.mod.json` generated for non-mod dependencies an API for ModMenu purposes to prevent large mods with many dependencies from cluttering the menu.
    
    * Changed from providing `modmenu:api` to `fabric-loom:generated` to be agnostic about third party mod usage.

--- src/main/java/net/fabricmc/loom/util/NestedJars.java
+++ src/main/java/net/fabricmc/loom/util/NestedJars.java
@@ -224,6 +224,10 @@ public class NestedJars {
 		jsonObject.addProperty("version", dependency.getModuleVersion());
 		jsonObject.addProperty("name", dependency.getModuleName());
 
+		JsonObject custom = new JsonObject();
+		custom.addProperty("fabric-loom:generated", true);
+		jsonObject.add("custom", custom);
+
 		return GSON.toJson(jsonObject);
 	}
 

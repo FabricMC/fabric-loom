c0cb877b0ceeff7b0d9f3624fe7593b8b799d566
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-12-03 22:17:53 +0200

    Add a dummy version of mcp.MethodsReturnNonnullByDefault to forgeInject

---
+++ src/forgeInject/java/mcp/MethodsReturnNonnullByDefault.java
@@ -0,0 +1,10 @@
+package mcp;
+
+/**
+ * A dummy class, required for some Forge classes to load.
+ *
+ * @deprecated Don't use this in your mods. JetBrains annotations are there for you.
+ */
+@Deprecated
+public @interface MethodsReturnNonnullByDefault {
+}

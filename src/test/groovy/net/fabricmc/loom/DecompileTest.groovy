87f3743b2423b59e40ca5a74e8641affffa57499
modmuss50 <modmuss50@gmail.com>
2021-03-06 11:40:07 +0000

    Rewrite tests to be easier to write and maintain (#361)

---
+++ src/test/groovy/net/fabricmc/loom/DecompileTest.groovy
@@ -0,0 +1,54 @@
+/*
+ * This file is part of fabric-loom, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016, 2017, 2018 FabricMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.fabricmc.loom
+
+import net.fabricmc.loom.util.ProjectTestTrait
+import spock.lang.Ignore
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+@Ignore // TODO this fails due to the plugin classpath being setup differently. We need to explore other ways to do this.
+class DecompileTest extends Specification implements ProjectTestTrait {
+	@Override
+	String name() {
+		"simple"
+	}
+
+	@Unroll
+	def "#decompiler decompile"() {
+		when:
+			def result = create(task)
+
+		then:
+			result.task(":${task}").outcome == SUCCESS
+
+		where:
+			decompiler 		| task
+			'fernflower'	| "genSources"
+			'cfr' 			| "genSourcesWithExperimentalCfr"
+	}
+}

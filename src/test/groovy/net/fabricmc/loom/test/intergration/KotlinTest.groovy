df96413cb278315db32c560c8c416b6dab63f70b
modmuss50 <modmuss50@gmail.com>
2021-04-04 17:48:13 +0100

    Disable KotlinTest on j16

--- src/test/groovy/net/fabricmc/loom/test/intergration/KotlinTest.groovy
+++ src/test/groovy/net/fabricmc/loom/test/intergration/KotlinTest.groovy
@@ -25,11 +25,13 @@
 package net.fabricmc.loom.test.intergration
 
 import net.fabricmc.loom.test.util.ProjectTestTrait
+import spock.lang.IgnoreIf
 import spock.lang.Specification
 import spock.lang.Unroll
 
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
+@IgnoreIf({ jvm.java16Compatible }) // Fails on J16 > due to https://youtrack.jetbrains.com/issue/KT-45566
 class KotlinTest extends Specification implements ProjectTestTrait {
 	@Override
 	String name() {

2f38c747a1c93893d0c797500c9d226b569d51b5
modmuss50 <modmuss50@gmail.com>
2021-05-12 20:31:05 +0100

    Catch all when merging jars

--- src/main/java/net/fabricmc/loom/configuration/providers/MinecraftProvider.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/MinecraftProvider.java
@@ -31,7 +31,6 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.zip.ZipError;
 
 import com.google.common.io.Files;
 import com.google.gson.GsonBuilder;
@@ -101,13 +100,13 @@ public class MinecraftProvider extends DependencyProvider {
 		if (!minecraftMergedJar.exists() || isRefreshDeps()) {
 			try {
 				mergeJars(getProject().getLogger());
-			} catch (ZipError e) {
+			} catch (Throwable e) {
 				HashedDownloadUtil.delete(minecraftClientJar);
 				HashedDownloadUtil.delete(minecraftServerJar);
 				minecraftMergedJar.delete();
 
 				getProject().getLogger().error("Could not merge JARs! Deleting source JARs - please re-run the command and move on.", e);
-				throw new RuntimeException();
+				throw e;
 			}
 		}
 	}

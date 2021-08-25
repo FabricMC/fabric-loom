FabricMC/fabric-loom

commit b320c4199a3aae41a83d5c336d45ecaac0499c36

Allow developer set the site where to download game resource.(0.9 branch) (#442)
* Add mirrors util and mirror configuration

* Fix NPE in MirrorConfiguration;
Add constants in Constants;
Rename Mirrors.java to MirrorUtil.java

* Bump gradle nightly version

* Apply mirror to download assets.
Need fix settings in LoomRepositoryPlugin.java

* Fix MojangMappingsSpec having the same hash version for each mc version.

I dont think this would be a real cause for concern as the jar would have had the mc version in it anyway.

* Revert "Fix MojangMappingsSpec having the same hash version for each mc version."

This reverts commit 61fa4d9f

* Remove MirrorConfiguration and move to MirrorUtil

* StyleCheck.

* Fix "IllegalStateException: The root project is not yet available for build."

* Remove some of the extra content.

Co-authored-by: BreakIce <suibing112233@outlook.com>
Co-authored-by: modmuss50 <modmuss50@gmail.com>

src/main/java/net/fabricmc/loom/LoomRepositoryPlugin.java
@@ -30,10 +30,13 @@ import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.artifacts.repositories.IvyArtifactRepository;
 import org.gradle.api.initialization.Settings;
 import org.gradle.api.invocation.Gradle;
+import org.gradle.api.plugins.ExtensionAware;
 import org.gradle.api.plugins.PluginAware;
 import org.jetbrains.annotations.NotNull;
 
 import net.fabricmc.loom.extension.LoomFiles;
+
+import net.fabricmc.loom.util.MirrorUtil;
 
 public class LoomRepositoryPlugin implements Plugin<PluginAware> {
 	@Override
@@ -63,11 +66,11 @@ public class LoomRepositoryPlugin implements Plugin<PluginAware> {
 		});
 		repositories.maven(repo -> {
 			repo.setName("Fabric");
-			repo.setUrl("https://maven.fabricmc.net/");
+			repo.setUrl(MirrorUtil.getFabricRepository((ExtensionAware) target));
 		});
 		repositories.maven(repo -> {
 			repo.setName("Mojang");
-			repo.setUrl("https://libraries.minecraft.net/");
+			repo.setUrl(MirrorUtil.getLibrariesBase((ExtensionAware) target));
 		});
 		repositories.mavenCentral();
 

src/main/java/net/fabricmc/loom/configuration/providers/MinecraftProviderImpl.java
@@ -43,6 +43,7 @@ import net.fabricmc.loom.configuration.providers.minecraft.ManifestVersion;
 import net.fabricmc.loom.configuration.providers.minecraft.MinecraftLibraryProvider;
 import net.fabricmc.loom.configuration.providers.minecraft.MinecraftVersionMeta;
 import net.fabricmc.loom.util.Constants;
+import net.fabricmc.loom.util.MirrorUtil;
 import net.fabricmc.loom.util.DownloadUtil;
 import net.fabricmc.loom.util.HashedDownloadUtil;
 import net.fabricmc.stitch.merge.JarMerger;
@@ -140,7 +141,7 @@ public class MinecraftProviderImpl extends DependencyProvider implements Minecra
 			}
 		} else {
 			getProject().getLogger().debug("Downloading version manifests");
-			DownloadUtil.downloadIfChanged(new URL(Constants.VERSION_MANIFESTS), versionManifestJson, getProject().getLogger());
+			DownloadUtil.downloadIfChanged(new URL(MirrorUtil.getVersionManifests(getProject())), versionManifestJson, getProject().getLogger());
 		}
 
 		String versionManifest = Files.asCharSource(versionManifestJson, StandardCharsets.UTF_8).read();
@@ -199,7 +200,7 @@ public class MinecraftProviderImpl extends DependencyProvider implements Minecra
 				return Optional.empty();
 			}
 		} else {
-			DownloadUtil.downloadIfChanged(new URL(Constants.EXPERIMENTAL_VERSIONS), experimentalVersionsJson, getProject().getLogger());
+			DownloadUtil.downloadIfChanged(new URL(MirrorUtil.getExperimentalVersions(getProject())), experimentalVersionsJson, getProject().getLogger());
 		}
 
 		String expVersionManifest = Files.asCharSource(experimentalVersionsJson, StandardCharsets.UTF_8).read();

src/main/java/net/fabricmc/loom/configuration/providers/minecraft/MinecraftNativesProvider.java
@@ -40,6 +40,7 @@ import org.zeroturnaround.zip.ZipUtil;
 import net.fabricmc.loom.LoomGradleExtension;
 import net.fabricmc.loom.LoomGradlePlugin;
 import net.fabricmc.loom.util.HashedDownloadUtil;
+import net.fabricmc.loom.util.MirrorUtil;
 
 public class MinecraftNativesProvider {
 	private final Project project;
@@ -93,7 +94,7 @@ public class MinecraftNativesProvider {
 			File libJarFile = library.relativeFile(jarStore);
 
 			if (!offline) {
-				HashedDownloadUtil.downloadIfInvalid(new URL(library.url()), libJarFile, library.sha1(), project.getLogger(), false);
+				HashedDownloadUtil.downloadIfInvalid(new URL(MirrorUtil.getLibrariesBase(project) + library.path()), libJarFile, library.sha1(), project.getLogger(), false);
 			}
 
 			if (!libJarFile.exists()) {

src/main/java/net/fabricmc/loom/configuration/providers/minecraft/assets/MinecraftAssetsProvider.java
@@ -43,7 +43,7 @@ import net.fabricmc.loom.LoomGradleExtension;
 import net.fabricmc.loom.LoomGradlePlugin;
 import net.fabricmc.loom.configuration.providers.MinecraftProviderImpl;
 import net.fabricmc.loom.configuration.providers.minecraft.MinecraftVersionMeta;
-import net.fabricmc.loom.util.Constants;
+import net.fabricmc.loom.util.MirrorUtil;
 import net.fabricmc.loom.util.HashedDownloadUtil;
 import net.fabricmc.loom.util.gradle.ProgressLogger;
 
@@ -117,7 +117,7 @@ public class MinecraftAssetsProvider {
 					final ProgressLogger[] progressLogger = new ProgressLogger[1];
 
 					try {
-						HashedDownloadUtil.downloadIfInvalid(new URL(Constants.RESOURCES_BASE + sha1.substring(0, 2) + "/" + sha1), file, sha1, project.getLogger(), true, () -> {
+						HashedDownloadUtil.downloadIfInvalid(new URL(MirrorUtil.getResourcesBase(project) + sha1.substring(0, 2) + "/" + sha1), file, sha1, project.getLogger(), true, () -> {
 							ProgressLogger logger = loggers.pollFirst();
 
 							if (logger == null) {

src/main/java/net/fabricmc/loom/util/Constants.java
@@ -37,6 +37,7 @@ public class Constants {
 	public static final String RESOURCES_BASE = "https://resources.download.minecraft.net/";
 	public static final String VERSION_MANIFESTS = "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json";
 	public static final String EXPERIMENTAL_VERSIONS = "https://maven.fabricmc.net/net/minecraft/experimental_versions.json";
+	public static final String FABRIC_REPOSITORY = "https://maven.fabricmc.net/";
 
 	public static final String SYSTEM_ARCH = System.getProperty("os.arch").equals("64") ? "64" : "32";
 

src/main/java/net/fabricmc/loom/util/MirrorUtil.java
@@ -1,0 +1,69 @@
+/*
+ * This file is part of fabric-loom, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2016-2021 FabricMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.fabricmc.loom.util;
+
+import org.gradle.api.plugins.ExtensionAware;
+
+public class MirrorUtil {
+	public static String getLibrariesBase(ExtensionAware aware) {
+		if (aware.getExtensions().getExtraProperties().has("loom_libraries_base")) {
+			return String.valueOf(aware.getExtensions().getExtraProperties().get("loom_libraries_base"));
+		}
+
+		return Constants.LIBRARIES_BASE;
+	}
+
+	public static String getResourcesBase(ExtensionAware aware) {
+		if (aware.getExtensions().getExtraProperties().has("loom_resources_base")) {
+			return String.valueOf(aware.getExtensions().getExtraProperties().get("loom_resources_base"));
+		}
+
+		return Constants.RESOURCES_BASE;
+	}
+
+	public static String getVersionManifests(ExtensionAware aware) {
+		if (aware.getExtensions().getExtraProperties().has("loom_version_manifests")) {
+			return String.valueOf(aware.getExtensions().getExtraProperties().get("loom_version_manifests"));
+		}
+
+		return Constants.VERSION_MANIFESTS;
+	}
+
+	public static String getExperimentalVersions(ExtensionAware aware) {
+		if (aware.getExtensions().getExtraProperties().has("loom_experimental_versions")) {
+			return String.valueOf(aware.getExtensions().getExtraProperties().get("loom_experimental_versions"));
+		}
+
+		return Constants.EXPERIMENTAL_VERSIONS;
+	}
+
+	public static String getFabricRepository(ExtensionAware aware) {
+		if (aware.getExtensions().getExtraProperties().has("loom_fabric_repository")) {
+			return String.valueOf(aware.getExtensions().getExtraProperties().get("loom_fabric_repository"));
+		}
+
+		return Constants.FABRIC_REPOSITORY;
+	}
+}

commit 673a35ba6201aabba0f817a8d3b676949adb585e

fix bad merge caused by #442

src/main/java/net/fabricmc/loom/LoomRepositoryPlugin.java
@@ -35,14 +35,13 @@ import org.gradle.api.plugins.PluginAware;
 import org.jetbrains.annotations.NotNull;
 
 import net.fabricmc.loom.extension.LoomFiles;
-
 import net.fabricmc.loom.util.MirrorUtil;
 
 public class LoomRepositoryPlugin implements Plugin<PluginAware> {
 	@Override
 	public void apply(@NotNull PluginAware target) {
 		if (target instanceof Settings settings) {
-			declareRepositories(settings.getDependencyResolutionManagement().getRepositories(), LoomFiles.create(settings));
+			declareRepositories(settings.getDependencyResolutionManagement().getRepositories(), LoomFiles.create(settings), (ExtensionAware) target);
 
 			// leave a marker so projects don't try to override these
 			settings.getGradle().getPluginManager().apply(LoomRepositoryPlugin.class);
@@ -51,7 +50,7 @@ public class LoomRepositoryPlugin implements Plugin<PluginAware> {
 				return;
 			}
 
-			declareRepositories(project.getRepositories(), LoomFiles.create(project));
+			declareRepositories(project.getRepositories(), LoomFiles.create(project), (ExtensionAware) target);
 		} else if (target instanceof Gradle) {
 			return;
 		} else {
@@ -59,18 +58,18 @@ public class LoomRepositoryPlugin implements Plugin<PluginAware> {
 		}
 	}
 
-	private void declareRepositories(RepositoryHandler repositories, LoomFiles files) {
+	private void declareRepositories(RepositoryHandler repositories, LoomFiles files, ExtensionAware target) {
 		repositories.maven(repo -> {
 			repo.setName("UserLocalRemappedMods");
 			repo.setUrl(files.getRemappedModCache());
 		});
 		repositories.maven(repo -> {
 			repo.setName("Fabric");
-			repo.setUrl(MirrorUtil.getFabricRepository((ExtensionAware) target));
+			repo.setUrl(MirrorUtil.getFabricRepository(target));
 		});
 		repositories.maven(repo -> {
 			repo.setName("Mojang");
-			repo.setUrl(MirrorUtil.getLibrariesBase((ExtensionAware) target));
+			repo.setUrl(MirrorUtil.getLibrariesBase(target));
 		});
 		repositories.mavenCentral();
 
61655765210cc083f816ca11835a06be7b3a2d86
shedaniel <daniel@shedaniel.me>
2021-08-31 21:54:17 +0800

    Make --refresh-dependencies work by creating the directory after clearing it (#482)

--- src/main/java/net/fabricmc/loom/configuration/providers/mappings/MappingsProviderImpl.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/mappings/MappingsProviderImpl.java
@@ -355,6 +355,8 @@ public class MappingsProviderImpl extends DependencyProvider implements Mappings
 			if (Files.exists(mappingsWorkingDir)) {
 				Files.walkFileTree(mappingsWorkingDir, new DeletingFileVisitor());
 			}
+
+			Files.createDirectories(mappingsWorkingDir);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}

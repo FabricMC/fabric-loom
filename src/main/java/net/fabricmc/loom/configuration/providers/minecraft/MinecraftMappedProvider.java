f3260866786e3512d33879444c7fbdd6c2e5091c
modmuss50 <modmuss50@gmail.com>
2021-09-20 22:02:41 +0100

    Revert "Update tiny remapper"
    This reverts commit e0d40442ac30bfd112f61bf5e65cbb1df25477b7.

--- src/main/java/net/fabricmc/loom/configuration/providers/minecraft/MinecraftMappedProvider.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/minecraft/MinecraftMappedProvider.java
@@ -31,7 +31,6 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Consumer;
-import java.util.regex.Pattern;
 
 import com.google.common.collect.ImmutableMap;
 import org.gradle.api.Project;
@@ -50,11 +49,6 @@ public class MinecraftMappedProvider extends DependencyProvider {
 			.put("javax/annotation/Nonnull", "org/jetbrains/annotations/NotNull")
 			.put("javax/annotation/concurrent/Immutable", "org/jetbrains/annotations/Unmodifiable")
 			.build();
-
-	/**
-	 * Matches the new local variable naming format introduced in 21w37a.
-	 */
-	private static final Pattern MC_LV_PATTERN = Pattern.compile("\\$\\$\\d+");
 
 	private File minecraftMappedJar;
 	private File minecraftIntermediaryJar;
@@ -141,7 +135,6 @@ public class MinecraftMappedProvider extends DependencyProvider {
 				.withMappings(out -> JSR_TO_JETBRAINS.forEach(out::acceptClass))
 				.renameInvalidLocals(true)
 				.rebuildSourceFilenames(true)
-				.invalidLvNamePattern(MC_LV_PATTERN)
 				.build();
 	}
 

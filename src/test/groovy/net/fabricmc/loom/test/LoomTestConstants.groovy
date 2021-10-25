13f4b29d124945e96754af7fca82f12463047fb1
modmuss50 <modmuss50@gmail.com>
2021-10-25 16:34:06 +0100

    Alter TransitiveAccessWidenerJarProcessor to use the same logic used in ModCompileRemapper for resolving mod jars.

--- src/test/groovy/net/fabricmc/loom/test/LoomTestConstants.groovy
+++ src/test/groovy/net/fabricmc/loom/test/LoomTestConstants.groovy
@@ -28,7 +28,7 @@ import org.gradle.util.GradleVersion
 
 class LoomTestConstants {
     public final static String DEFAULT_GRADLE = GradleVersion.current().getVersion()
-    public final static String PRE_RELEASE_GRADLE = "7.4-20211011231946+0000"
+    public final static String PRE_RELEASE_GRADLE = "7.4-20211023222429+0000"
 
     public final static String[] STANDARD_TEST_VERSIONS = [DEFAULT_GRADLE, PRE_RELEASE_GRADLE]
 }

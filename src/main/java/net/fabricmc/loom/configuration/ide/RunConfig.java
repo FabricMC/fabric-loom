1c9d4e54340abb25ae7721309d5ebc56a5c5ceab
Sebastian Hartte <shartte@users.noreply.github.com>
2021-12-04 16:28:49 +0100

    Only quote program arguments if they actually contain spaces. Fixing arguments like "--username" not being reocngized by MC.

--- src/main/java/net/fabricmc/loom/configuration/ide/RunConfig.java
+++ src/main/java/net/fabricmc/loom/configuration/ide/RunConfig.java
@@ -209,7 +209,12 @@ public class RunConfig {
 			}
 
 			first = false;
-			sb.append("\"").append(arg).append("\"");
+
+			if (arg.contains(" ")) {
+				sb.append("\"").append(arg).append("\"");
+			} else {
+				sb.append(arg);
+			}
 		}
 
 		return sb.toString();

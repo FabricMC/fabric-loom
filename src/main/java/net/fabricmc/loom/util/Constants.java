56f957b65c81c5b1caf3ce4a557a4a23f1dee95c
modmuss50 <modmuss50@gmail.com>
2021-12-04 14:27:36 +0000

    Java 17 and update libs. Closes #545

--- src/main/java/net/fabricmc/loom/util/Constants.java
+++ src/main/java/net/fabricmc/loom/util/Constants.java
@@ -105,7 +105,7 @@ public class Constants {
 			public static final String MIXIN_COMPILE_EXTENSIONS = "0.4.6";
 			public static final String DEV_LAUNCH_INJECTOR = "0.2.1+build.8";
 			public static final String TERMINAL_CONSOLE_APPENDER = "1.2.0";
-			public static final String JETBRAINS_ANNOTATIONS = "22.0.0";
+			public static final String JETBRAINS_ANNOTATIONS = "23.0.0";
 
 			private Versions() {
 			}

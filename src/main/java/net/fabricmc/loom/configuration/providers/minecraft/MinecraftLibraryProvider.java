c56206cc2a8d998fcde7528ae3fd9b27d6184768
Cat Core <34719527+arthurbambou@users.noreply.github.com>
2022-03-16 23:36:13 +0100

    Update MinecraftLibraryProvider.java

--- src/main/java/net/fabricmc/loom/configuration/providers/minecraft/MinecraftLibraryProvider.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/minecraft/MinecraftLibraryProvider.java
@@ -60,7 +60,7 @@ public class MinecraftLibraryProvider {
 
 			if (library.isValidForOS() && !library.hasNatives() && library.artifact() != null) {
 				// 1.4.7 contains an LWJGL version with an invalid maven pom, set the metadata sources to not use the pom for this version.
-				if ("org.lwjgl.lwjgl:lwjgl:2.9.1-nightly-20130708-debug3".equals(library.name())) {
+				if ("org.lwjgl.lwjgl:lwjgl:2.9.1-nightly-20130708-debug3".equals(library.name()) || "org.lwjgl.lwjgl:lwjgl:2.9.1-nightly-20131017".equals(library.name())) {
 					LoomRepositoryPlugin.setupForLegacyVersions(project);
 				} else if (library.name().startsWith("org.ow2.asm:asm-all")) {
 					// Don't want asm-all, use the modern split version.

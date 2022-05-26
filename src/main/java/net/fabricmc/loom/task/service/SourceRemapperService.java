07ded449eeff2dcdb49f7208eb3e8db12fd0bacb
modmuss50 <modmuss50@gmail.com>
2022-05-26 11:21:53 +0100

    Update mercury and set source compatibility version to 17

--- src/main/java/net/fabricmc/loom/task/service/SourceRemapperService.java
+++ src/main/java/net/fabricmc/loom/task/service/SourceRemapperService.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of fabric-loom, licensed under the MIT License (MIT).
  *
- * Copyright (c) 2021 FabricMC
+ * Copyright (c) 2021-2022 FabricMC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -42,6 +42,7 @@ import org.slf4j.LoggerFactory;
 
 import net.fabricmc.loom.LoomGradleExtension;
 import net.fabricmc.loom.task.RemapSourcesJarTask;
+import net.fabricmc.loom.util.Constants;
 import net.fabricmc.loom.util.DeletingFileVisitor;
 import net.fabricmc.loom.util.FileSystemUtil;
 import net.fabricmc.loom.util.SourceRemapper;
@@ -122,6 +123,7 @@ public final class SourceRemapperService implements SharedService {
 	private Mercury createMercury() {
 		var mercury = new Mercury();
 		mercury.setGracefulClasspathChecks(true);
+		mercury.setSourceCompatibility(Constants.MERCURY_SOURCE_VERSION);
 
 		try {
 			mercury.getProcessors().add(MercuryRemapper.create(getMappings()));

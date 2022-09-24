5ae24af98bc1af2f67133e5a266e07d40c3c0332
modmuss50 <modmuss50@gmail.com>
2022-09-24 21:02:49 +0100

    Fix incorrect Mixin message argument key.

--- src/main/java/net/fabricmc/loom/build/mixin/AnnotationProcessorInvoker.java
+++ src/main/java/net/fabricmc/loom/build/mixin/AnnotationProcessorInvoker.java
@@ -108,7 +108,7 @@ public abstract class AnnotationProcessorInvoker<T extends Task> {
 				checkPattern(key, MSG_KEY_PATTERN);
 				checkPattern(value, MSG_VALUE_PATTERN);
 
-				args.put("AMSG_" + key, value);
+				args.put("MSG_" + key, value);
 			});
 
 			project.getLogger().debug("Outputting refmap to dir: " + getRefmapDestinationDir(task) + " for compile task: " + task);

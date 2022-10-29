c281320e2ab9956ce0d4909e0e5da2faebde30cd
modmuss50 <modmuss50@gmail.com>
2022-10-29 15:37:03 +0100

    More test fixes.

--- src/test/groovy/net/fabricmc/loom/test/util/MockMavenServerTrait.groovy
+++ src/test/groovy/net/fabricmc/loom/test/util/MockMavenServerTrait.groovy
@@ -37,7 +37,6 @@ trait MockMavenServerTrait {
         println "Maven server path: ${testMavenDir.absolutePath}"
 
         server = Javalin.create { config ->
-            config.enableDevLogging()
         }.start(mavenServerPort)
 
         /**

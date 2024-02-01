04f35905f2ecd818265b664b9078ed322df3a040
Space Walker <spacedoesrs@gmail.com>
2024-02-01 20:33:22 +0100

    another checkstyle fix

--- src/main/java/net/fabricmc/loom/api/LoomGradleExtensionAPI.java
+++ src/main/java/net/fabricmc/loom/api/LoomGradleExtensionAPI.java
@@ -170,7 +170,7 @@ public interface LoomGradleExtensionAPI {
 	/**
 	 * When true loom will merge the client and server jars before remapping them,
 	 * otherwise the client and server jars will be remapped separately and then merged.
-	 * 
+	 *
 	 * @return whether the obfuscated client and server jars can be merged
 	 */
 	Property<Boolean> canMergeObfuscatedJars();

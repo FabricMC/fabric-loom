4084fa3eb9b6dd4003e108ff47efe56b06aa7ff1
modmuss50 <modmuss50@gmail.com>
2024-03-19 22:03:01 +0000

    Add classname to stracktrace when Kotlin remapping fails. Closes #1045

--- src/main/kotlin/net/fabricmc/loom/kotlin/remapping/KotlinMetadataRemappingClassVisitor.kt
+++ src/main/kotlin/net/fabricmc/loom/kotlin/remapping/KotlinMetadataRemappingClassVisitor.kt
@@ -57,7 +57,11 @@ class KotlinMetadataRemappingClassVisitor(private val remapper: Remapper, next: 
         var result: AnnotationVisitor? = super.visitAnnotation(descriptor, visible)
 
         if (descriptor == ANNOTATION_DESCRIPTOR && result != null) {
-            result = KotlinClassMetadataRemappingAnnotationVisitor(remapper, result, className)
+            try {
+                result = KotlinClassMetadataRemappingAnnotationVisitor(remapper, result, className)
+            } catch (e: Exception) {
+                throw RuntimeException("Failed to remap Kotlin metadata annotation in class $className", e)
+            }
         }
 
         return result

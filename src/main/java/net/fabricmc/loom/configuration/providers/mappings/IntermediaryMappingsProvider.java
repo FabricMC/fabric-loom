648a059d26abed280b378b60f4c32ded7d4335b8
ev chang <wyvestbusiness@gmail.com>
2024-09-03 13:11:37 -0400

    Add SHA1 suffix to intermediaries (#1141)
    * Add SHA1 suffix to intermediaries
    
    Having two or more different intermediaries for one version breaks; we use this to differentiate between intermediaries.
    
    * Only add SHA1 to name if intermediary url is different from default
    
    * Format code and remove duplicated method

--- src/main/java/net/fabricmc/loom/configuration/providers/mappings/IntermediaryMappingsProvider.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/mappings/IntermediaryMappingsProvider.java
@@ -25,6 +25,7 @@
 package net.fabricmc.loom.configuration.providers.mappings;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -47,6 +48,7 @@ import org.slf4j.LoggerFactory;
 
 import net.fabricmc.loom.api.mappings.intermediate.IntermediateMappingsProvider;
 import net.fabricmc.loom.extension.LoomGradleExtensionApiImpl;
+import net.fabricmc.loom.util.Checksum;
 
 @ApiStatus.Internal
 public abstract class IntermediaryMappingsProvider extends IntermediateMappingsProviderInternal {
@@ -107,6 +109,15 @@ public abstract class IntermediaryMappingsProvider extends IntermediateMappingsP
 
 	@Override
 	public @NotNull String getName() {
+		final String encodedMcVersion = UrlEscapers.urlFragmentEscaper().escape(getMinecraftVersion().get());
+		final String urlRaw = getIntermediaryUrl().get();
+
+		if (!LoomGradleExtensionApiImpl.DEFAULT_INTERMEDIARY_URL.equals(urlRaw)) {
+			final String url = getIntermediaryUrl().get().formatted(encodedMcVersion);
+
+			return NAME + "-" + Checksum.sha1Hex(url.getBytes(StandardCharsets.UTF_8));
+		}
+
 		return NAME;
 	}
 }

214a9d390228d9d1c3e7d6b18ebb7bf59480089c
modmuss50 <modmuss50@gmail.com>
2025-01-30 09:24:08 +0000

    Fix build

--- src/test/groovy/net/fabricmc/loom/test/integration/LayeredMappingsTest.groovy
+++ src/test/groovy/net/fabricmc/loom/test/integration/LayeredMappingsTest.groovy
@@ -24,9 +24,10 @@
 
 package net.fabricmc.loom.test.integration
 
-import net.fabricmc.loom.test.util.GradleProjectTestTrait
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import net.fabricmc.loom.test.util.GradleProjectTestTrait
 
 import static net.fabricmc.loom.test.LoomTestConstants.DEFAULT_GRADLE
 import static net.fabricmc.loom.test.LoomTestConstants.STANDARD_TEST_VERSIONS
@@ -59,27 +60,27 @@ class LayeredMappingsTest extends Specification implements GradleProjectTestTrai
 
 		where:
 		layer << [
-				// Only mojang mappings
-				"""
+			// Only mojang mappings
+			"""
                 officialMojangMappings()
             """,
-				// Yarn on top of Mojmap
-				"""
+			// Yarn on top of Mojmap
+			"""
                 officialMojangMappings()
                 mappings("net.fabricmc:yarn:1.21.4+build.8:v2")
             """,
-				// Mojmap on top of yarn
-				"""
+			// Mojmap on top of yarn
+			"""
                 mappings("net.fabricmc:yarn:1.21.4+build.8:v2")
                 officialMojangMappings()
             """,
-				//  Mojmap with parchment
-				"""
+			//  Mojmap with parchment
+			"""
                 officialMojangMappings()
                 parchment("org.parchmentmc.data:parchment-1.21.4:2025.01.19@zip")
             """,
-				// Yarn on top of Mojmap with parchment
-				"""
+			// Yarn on top of Mojmap with parchment
+			"""
                 officialMojangMappings()
                 parchment("org.parchmentmc.data:parchment-1.21.4:2025.01.19@zip")
                 mappings("net.fabricmc:yarn:1.21.4+build.8:v2")

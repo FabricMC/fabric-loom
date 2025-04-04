169a9cfa7f0840e8ff1f9bdb0af94ee87bbf3259
modmuss50 <modmuss50@gmail.com>
2025-04-04 16:38:52 +0100

    Unit test fixes

--- src/test/groovy/net/fabricmc/loom/test/util/CertificateUtils.groovy
+++ src/test/groovy/net/fabricmc/loom/test/util/CertificateUtils.groovy
@@ -33,7 +33,10 @@ import org.bouncycastle.asn1.DERIA5String
 import org.bouncycastle.asn1.x500.X500Name
 import org.bouncycastle.asn1.x509.CRLNumber
 import org.bouncycastle.asn1.x509.CRLReason
+import org.bouncycastle.asn1.x509.DistributionPointName
 import org.bouncycastle.asn1.x509.Extension
+import org.bouncycastle.asn1.x509.GeneralName
+import org.bouncycastle.asn1.x509.GeneralNames
 import org.bouncycastle.cert.jcajce.JcaX509CRLConverter
 import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter
 import org.bouncycastle.cert.jcajce.JcaX509ExtensionUtils

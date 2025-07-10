9a2f772425496ab2b2630d8ff769ba61d8e61912
modmuss50 <modmuss50@gmail.com>
2025-07-10 19:17:30 +0100

    Disable jar verification by default

--- src/main/java/net/fabricmc/loom/configuration/providers/minecraft/MinecraftProvider.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/minecraft/MinecraftProvider.java
@@ -122,7 +122,7 @@ public abstract class MinecraftProvider {
 	}
 
 	private void verifyJars() throws IOException, SignatureVerificationFailure {
-		if (GradleUtils.getBooleanProperty(getProject(), Constants.Properties.DISABLE_MINECRAFT_VERIFICATION)) {
+		if (GradleUtils.getBooleanProperty(getProject(), Constants.Properties.DISABLE_MINECRAFT_VERIFICATION, true)) {
 			LOGGER.info("Skipping Minecraft jar verification!");
 		}
 

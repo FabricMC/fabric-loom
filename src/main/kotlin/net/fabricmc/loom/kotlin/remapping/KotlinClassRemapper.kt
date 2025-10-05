efacc2b90d0e8c5becece3d489143349c2b4efdc
modmuss50 <modmuss50@gmail.com>
2025-10-05 16:58:27 +0100

    Fix remapping Kotlin classes containing $.
    Closes #1363

--- src/main/kotlin/net/fabricmc/loom/kotlin/remapping/KotlinClassRemapper.kt
+++ src/main/kotlin/net/fabricmc/loom/kotlin/remapping/KotlinClassRemapper.kt
@@ -88,7 +88,13 @@ class KotlinClassRemapper(
 
     private fun remap(name: ClassName): ClassName {
         val local = name.isLocalClassName()
-        val remapped = remapper.map(name.toJvmInternalName()).replace('$', '.')
+
+        // Ensure that none inner classes with names containing $ are persisted.
+        // See: https://github.com/FabricMC/fabric-loom/issues/1363 fix suggested by fan87
+        val normalizedName = name.replace('$', '\n')
+        val remapped = remapper.map(normalizedName.toJvmInternalName())
+            .replace('$', '.')
+            .replace('\n', '$')
 
         if (local) {
             return ".$remapped"

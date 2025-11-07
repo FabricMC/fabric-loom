61cc1b19403f802396bfe83272d698eaf4f1f096
modmuss50 <modmuss50@gmail.com>
2025-11-07 21:05:45 +0000

    Fix build

--- src/test/groovy/net/fabricmc/loom/test/integration/noRemap/SimpleDebofTest.groovy
+++ src/test/groovy/net/fabricmc/loom/test/integration/noRemap/SimpleDebofTest.groovy
@@ -59,7 +59,10 @@ class SimpleDebofTest extends Specification implements GradleProjectTestTrait {
 		sourceFile.text = src
 
 		when:
-		def result = gradle.run(tasks: ["build", "configureClientLaunch"])
+		def result = gradle.run(tasks: [
+			"build",
+			"configureClientLaunch"
+		])
 
 		then:
 		result.task(":build").outcome == SUCCESS

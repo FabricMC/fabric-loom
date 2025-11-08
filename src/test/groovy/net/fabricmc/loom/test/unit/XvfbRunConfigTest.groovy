7534117595df31c8fb1f6b2319504e286d0c9e85
zOnlyKroks <finnrades@gmail.com>
2025-11-08 09:36:17 +0100

    Remove unused import

--- src/test/groovy/net/fabricmc/loom/test/unit/XvfbRunConfigTest.groovy
+++ src/test/groovy/net/fabricmc/loom/test/unit/XvfbRunConfigTest.groovy
@@ -25,7 +25,6 @@
 package net.fabricmc.loom.test.unit
 
 import org.gradle.api.Project
-import org.gradle.api.tasks.SourceSet
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
 

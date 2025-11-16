6b62e336c9a155e865958ffd05f887b955d9e124
opekope2 <20487556+opekope2@users.noreply.github.com>
2025-11-16 23:39:04 +0100

    Document remapJsrAnnotationsToJetBrains

--- src/main/java/net/fabricmc/loom/api/LoomGradleExtensionAPI.java
+++ src/main/java/net/fabricmc/loom/api/LoomGradleExtensionAPI.java
@@ -257,6 +257,15 @@ public interface LoomGradleExtensionAPI {
 
 	boolean areEnvironmentSourceSetsSplit();
 
+	/**
+	 * When enabled, Loom remaps JSR {@code Nullable}, {@code Nonnull}, and {@code Immutable} annotations to their JetBrains counterparts in the Minecraft JAR.
+	 * <p>When disabled, Loom keeps JSR annotations as-is, and remaps any JetBrains {@code Nullable}, {@code NotNull}, and {@code Unmodifiable} annotations to their JSR counterparts in the Minecraft JAR.
+	 * <p>This has no effect on Minecraft versions that solely use JSpecify annotations.
+	 *
+	 * <p>Default: true
+	 *
+	 * @return the property controlling the remapping of JSR annotations
+	 */
 	Property<Boolean> getRemapJsrAnnotationsToJetBrains();
 
 	Property<Boolean> getRuntimeOnlyLog4j();

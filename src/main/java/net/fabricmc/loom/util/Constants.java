4894da5ff4ad27c13b3f8dc2b6e822420c6110e8
modmuss50 <modmuss50@gmail.com>
2026-07-17 11:56:07 +0100

    Set real client id

--- src/main/java/net/fabricmc/loom/util/Constants.java
+++ src/main/java/net/fabricmc/loom/util/Constants.java
@@ -34,7 +34,7 @@ public class Constants {
 	public static final String EXPERIMENTAL_VERSIONS = "https://maven.fabricmc.net/net/minecraft/experimental_versions.json";
 	public static final String FABRIC_REPOSITORY = "https://maven.fabricmc.net/";
 	public static final String DLI_ENTRYPOINT = "net.fabricmc.devlaunchinjector.Main";
-	public static final String MICROSOFT_CLIENT_ID = "";
+	public static final String MICROSOFT_CLIENT_ID = "f8ebf47c-dffd-4ee4-b27e-c293f3a887a8";
 
 	public static final int ASM_VERSION = Opcodes.ASM9;
 	public static final String RELEASE_TIME_1_21_11_UNOBFUSCATED_SNAPSHOTS = "2025-11-01T00:00:00+00:00";
